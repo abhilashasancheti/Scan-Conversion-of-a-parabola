499e931df40c6ce0bc84c23147f7d0fab85eaf04
Abhilasha Sancheti <abhilashasancheti@users.noreply.github.com>
2016-08-29 22:21:57 +0530

    Add files via upload
    added files to scan convert parabola

---
+++ Parabola.cpp
@@ -0,0 +1,289 @@
+#include <math.h>
+#include <windows.h>
+#include "Parabola.h"
+
+DRAWING_DATA gDrawData; // global data
+void drawParabola(int s);   //declaration of function to draw parabola
+LRESULT CALLBACK DlgAxis(HWND hdlg,UINT mess,WPARAM more,LPARAM pos);
+
+void reDraw(HWND hwnd)
+{
+  InvalidateRect(hwnd, NULL, 1);
+}
+/* for highlighting the point which is the center.*/
+void drawPoint(int x, int y)
+{ 
+ Ellipse(gDrawData.hdcMem,x-2,y-2,x+2,y+2);
+}
+/* setup menu depending upon the mode
+enables and disables the items in the menu */
+void setupMenus(HWND hwnd)
+{
+  HMENU hMenu;
+  
+  hMenu=GetMenu(hwnd);
+  
+  switch (gDrawData.drawMode)
+  {
+    case READY_MODE :
+      // enable parabola menu
+      EnableMenuItem(hMenu, ID_PARABOLA,
+                     MF_BYCOMMAND|MF_ENABLED);
+      break;
+    case DRAW_MODE :
+    case DRAWN_MODE :
+      // disable parabola menu
+      EnableMenuItem(hMenu, ID_PARABOLA,
+                     MF_BYCOMMAND|MF_GRAYED);
+      break;
+  }
+}
+/* sets the drawing mode.*/
+void setDrawMode(MODE mode, HWND hwnd)
+{
+  gDrawData.drawMode = mode;
+  setupMenus(hwnd);
+}
+/*creates memory bitmap */
+void createMemoryBitmap(HDC hdc)
+{
+  gDrawData.hdcMem = CreateCompatibleDC(hdc);
+  gDrawData.hbmp = CreateCompatibleBitmap(hdc, 
+    gDrawData.maxBoundary.cx, gDrawData.maxBoundary.cy);
+  SelectObject(gDrawData.hdcMem, gDrawData.hbmp);
+  PatBlt(gDrawData.hdcMem, 0, 0, gDrawData.maxBoundary.cx, 
+         gDrawData.maxBoundary.cy, PATCOPY);
+}
+/*initializes the centre point which is selected on the drawing panel*/
+void initialize(HWND hwnd, HDC hdc)
+{
+  gDrawData.hDrawPen=CreatePen(PS_SOLID, 1, RGB(255, 0, 0));
+
+  gDrawData.maxBoundary.cx = GetSystemMetrics(SM_CXSCREEN);
+  gDrawData.maxBoundary.cy = GetSystemMetrics(SM_CYSCREEN);
+
+  createMemoryBitmap(hdc);
+  setDrawMode(READY_MODE, hwnd);
+}
+/* cleans the drawing area.*/
+void cleanup()
+{ 
+  DeleteDC(gDrawData.hdcMem);
+}
+/* resets the x and y point at the origin */
+void reset(HWND hwnd)
+{
+  gDrawData.centre.x = gDrawData.centre.y = 0;
+
+  gDrawData.drawMode = READY_MODE;
+
+  PatBlt(gDrawData.hdcMem, 0, 0, gDrawData.maxBoundary.cx, 
+         gDrawData.maxBoundary.cy, PATCOPY);
+
+  reDraw(hwnd);
+  setupMenus(hwnd);
+}
+/*Plots the symmetry points with offsets as the point which we select on the drawing area.*/
+void plot_sympoint(int ex, int ey, COLORREF clr)
+{
+  int cx = gDrawData.centre.x;
+  int cy = gDrawData.centre.y;
+
+  SetPixel(gDrawData.hdcMem, ex+cx,cy-ey, clr);
+ // SetPixel(gDrawData.hdcMem, -ex+cx,cy-ey, clr);
+//  SetPixel(gDrawData.hdcMem, -ex+cx,cy+ey, clr);
+  SetPixel(gDrawData.hdcMem, ex+cx,cy+ey, clr);
+}
+
+void addPoint(HWND hwnd, int x, int y)
+{
+  switch (gDrawData.drawMode)
+  {
+    case DRAW_MODE:
+      /* the coordinates of the centre is stored 
+         and the parabola is drawn */
+      SelectObject(gDrawData.hdcMem, gDrawData.hDrawPen);
+      drawPoint(x,y);
+      gDrawData.centre.x = x;
+      gDrawData.centre.y = y;
+      drawParabola(gDrawData.scale);
+      setDrawMode(DRAWN_MODE, hwnd);
+      reDraw(hwnd);
+      break;
+    case DRAWN_MODE:
+      MessageBox(hwnd,
+       "Parabola already drawn, now you can clear the area", 
+          "Warning",MB_OK);
+      break;
+    default:
+      break;
+  }
+}
+
+void drawImage(HDC hdc)
+{
+  BitBlt(hdc, 0, 0, gDrawData.maxBoundary.cx, 
+    gDrawData.maxBoundary.cy, gDrawData.hdcMem, 
+    0, 0, SRCCOPY);
+}
+
+void processLeftButtonDown(HWND hwnd, int x, int y)
+{
+  addPoint(hwnd,x,y);
+}
+
+void processCommand(int cmd, HWND hwnd)
+{
+  int response;
+  switch(cmd)
+  {
+    case ID_CLEAR:
+      reset(hwnd);
+      setDrawMode(READY_MODE, hwnd);
+      break;
+    case ID_PARABOLA:
+      setDrawMode(DRAW_MODE, hwnd);
+     // DialogBox(NULL,"MyDB",hwnd,(DLGPROC)DlgAxis);
+      break;
+    case ID_EXIT:
+        response=MessageBox(hwnd,
+          "Quit the program?", "EXIT", 
+          MB_YESNO);
+        if(response==IDYES) 
+            PostQuitMessage(0);
+        break;
+    default:
+      break;
+  }
+}
+/* main logic to draw the parabola. */
+void drawParabola(int s)
+{ s=1;
+ int x,y;
+  x=0*s;
+  y=0*s;
+  /* plot the point given by x and y*/
+  plot_sympoint(x*s,y*s, RGB(0,0,0));
+  double d= 1/2.0;  /* initial decision parameter at x=0 and y=0 calculated using mid-point (1/2,1) in y2-x equation*/ 
+  while(y<=(1/2)){
+       /* in region 1 y<=1/2 y is always incremented by 1 */
+        plot_sympoint(x*s,y*s, RGB(0,0,0));    
+  	if(d<0){
+  		/* if d<0 then mid-point lie inside the parabola and we take xk+1 = xk*/
+  		d=d+2*(double)y+3.0;
+  		y=y+1;
+	  }
+	  else
+	  { /* if d>=0 then midpoint lie outside the parabola and we take xk+1 = xk + 1*/
+	  		d=d+(2.0*y)+2.0;
+	  		y=y+1;
+	  		x=x+1;
+	  }
+	 
+  }
+  
+  /* calculating decision parameter for the new region*/
+  d= (double)((double)y+1/2.0)*((double)y+1/2.0)-(double)((double)x+1.0);
+  while((y)<=10)  
+  {   plot_sympoint(x*s,y*s, RGB(0,0,0));
+    /* in region 2  y>=1/2 x is always incremented by 1*/
+    if(d<=0)
+    {
+      /*choose E if d<=0 mid-point lie inside the parabola and we take yk+1 = yk + 1*/
+      d=d+1.0+(2*(double)y);
+      x=x+1;
+      y=y+1;
+    }
+    else                              
+    {
+      /*choose SE if d>0 mid-point lie outside the parabola and we take yk+1 = yk*/
+      d=d-1.0;
+      x=x+1;
+     
+    }
+   
+  }
+}
+/* gets user data given in the dialogue*/
+LRESULT CALLBACK DlgAxis(HWND hdlg,UINT mess,WPARAM more,LPARAM pos)
+{
+  char str[20];
+  switch(mess)
+  {
+    case WM_COMMAND:
+      switch(more)
+      {
+        case ID_OK:
+         GetDlgItemText(hdlg,ID_EB1,str,20);
+          gDrawData.scale=(long int)(atof(str));
+        /*  GetDlgItemText(hdlg,ID_EB2,str,20);
+          gDrawData.iMinorDiameter=(long int)(atof(str))/2;
+          if(gDrawData.iMajorDiameter < 5 || 
+             gDrawData.iMajorDiameter > 700 ||
+             gDrawData.iMinorDiameter < 5 || 
+             gDrawData.iMinorDiameter > 400)
+          {
+           MessageBox(hdlg,
+              "Diameter should be between 5 and 400.", 
+             "Warning!",MB_ICONERROR);
+          } */
+          if(gDrawData.scale <0)
+           {
+           MessageBox(hdlg,
+              "Scaling should be positive", 
+             "Warning!",MB_ICONERROR);
+          }
+          EndDialog(hdlg,TRUE);
+          return 1;
+          break;
+
+        case ID_CANCEL:
+          EndDialog(hdlg,TRUE);
+          break;
+      }
+      break;
+    default:
+      break;
+  }
+  return 0;
+}
+/* initializes the variables , paints the image depending upon the message*/
+LRESULT CALLBACK WindowF(HWND hwnd,UINT message,WPARAM wParam,
+                         LPARAM lParam) 
+{
+  HDC hdc;
+  PAINTSTRUCT ps;
+  int x,y;
+
+  switch(message)
+  {
+    case WM_CREATE:
+      hdc = GetDC(hwnd);
+      initialize(hwnd, hdc);
+      ReleaseDC(hwnd, hdc);
+      break;
+    
+    case WM_COMMAND:
+      processCommand(LOWORD(wParam), hwnd);
+      break;
+
+    case WM_LBUTTONDOWN:
+      x = LOWORD(lParam);
+      y = HIWORD(lParam);
+      processLeftButtonDown(hwnd, x,y);
+      break;
+
+    case WM_PAINT:
+      hdc = BeginPaint(hwnd, &ps);
+      drawImage(hdc);
+      EndPaint(hwnd, &ps);
+      break;
+
+    case WM_DESTROY:
+      cleanup();
+      PostQuitMessage(0);
+      break;
+  }
+  // Call the default window handler
+  return DefWindowProc(hwnd, message, wParam, lParam);
+}
